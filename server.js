982350b8ea1dc54aab7212b2deeb3fcb0163f82b
viswapriya <random@gmail.com>
2026-09-19 00:46:16 +0530

    Initial commit for Node.js REST API CI/CD lab

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/status", (req, res) => {
+    res.json({
+        status: "success",
+        message: "Node.js REST API is running successfully"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
